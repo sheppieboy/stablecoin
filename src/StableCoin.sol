f52e6e34e479ca0b5a4f24a78953eddbb121d4e7
Luke Sheppard <sheppiegit@gmail.com>
2023-07-09 16:05:09 -0400

    Added burn function to the stable coin contract.

--- src/StableCoin.sol
+++ src/StableCoin.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/extensions";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 /**
  * @title StableCoin
  * @author Luke Sheppard
@@ -10,6 +13,22 @@ pragma solidity 0.8.18;
  * This contract meant to be giverned by DSCEngine. This contract is the ERC20 implementation of the
  * stable coin
  */
-contract StableCoin {
 
+error BalanceMustbeMoreThanZero();
+error BurnAmountExceedsBalance();
+
+contract StableCoin is ERC20Burnable, Ownable {
+    constructor() ERC20("Decentralized StableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert BalanceMustbeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
 }
