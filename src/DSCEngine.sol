c35efb52c322310c98b46cf6508864fa9b4a1d76
Luke Sheppard <sheppiegit@gmail.com>
2023-07-09 16:42:06 -0400

    Initial layout of functions for the DSCEngine.

---
+++ src/DSCEngine.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+/**
+ * @title DSCEngine
+ * @author
+ *
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token = $1 USD peg
+ * This stablecoin has the properties:
+ * - Exogenpus Collateral
+ * - Dollar Pegged
+ * - Algorithhmically Stable
+ *
+ * It is similiar to DAI if DAI has no governance, no fees, and was only backed by wETH and wBTC
+ *
+ * @notice This contract is the core of the decentralized stablecoin system. It handles all the logic for minting and redeeming
+ * DSC, as well as depositing and withdrawing collateral.
+ *
+ * @notice This contract is loosely based on teh MakerDAO DSS (DAI) System.
+ */
+contract DSCEngine {
+    /**
+     * Function to deposit collateral and recieve an equivalent stablecoin amount
+     */
+    function depositCollateralAndMintStableCoin() external {}
+
+    /**
+     * function to deposit collateral to increase the current collateralization of position
+     * i.e. a top function for a user's position
+     */
+    function depositCollateral() external {}
+
+    /**
+     * function to redeem the collateral in exchange for the equivalent stablecoin amount
+     */
+    function redeemCollateralForStableCoin() external {}
+
+    /**
+     * function to redeemSomeCallateral to reduce collateral sizing for DSC position
+     */
+    function redeemCollateral() external {}
+
+    /**
+     * function to burn stablecoin
+     */
+
+    function burn() external {}
+
+    /**
+     * function to liquidate users position to rebalance the protocol
+     */
+
+    function liquidate() external {}
+
+    /**
+     * function to view the health of a user's position and see the health of their position
+     */
+    function getHealthFactor() external view {}
+
+    /**
+     * function mintStableCoin, a function thats mints the stablecoin
+     */
+    function mintDSC() external {}
+}
