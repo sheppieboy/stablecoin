bdf06739dc6a93c88771c3c2dd097b275fad3154
Luke Sheppard <sheppiegit@gmail.com>
2023-07-09 15:52:59 -0400

    initial project setup.  Added the ERC20 stablecoin initial contract and removed example cpcontracts from project.

---
+++ src/StableCoin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+/**
+ * @title StableCoin
+ * @author Luke Sheppard
+ * Collateral: Exogenous (BTC and ETH)
+ * Relative Stability: Pegged to USD
+ *
+ * This contract meant to be giverned by DSCEngine. This contract is the ERC20 implementation of the
+ * stable coin
+ */
+contract StableCoin {
+
+}
