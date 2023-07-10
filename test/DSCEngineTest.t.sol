6a2743fb62745e3b112a081d8c2c25a3663fc72e
Luke Sheppard <sheppiegit@gmail.com>
2023-07-10 15:23:57 -0400

    Added a test to check that a token not allowed cnanot deposit.

--- test/DSCEngineTest.t.sol
+++ test/DSCEngineTest.t.sol
@@ -3,17 +3,24 @@ pragma solidity 0.8.18;
 
 import "forge-std/Test.sol";
 import "../src/DSCEngine.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract DSCEngineTest is Test {
     uint256 private mainnetFork;
     DSCEngine private engine;
+    address public WETH;
+    address public WBTC;
+    address public dai;
+    address public randomUser;
 
     function setUp() public {
         mainnetFork = vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
         //add WETH and WBTC token address to tokenAddresses array
         address[] memory tokenAddresses = new address[](2);
-        tokenAddresses[0] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-        tokenAddresses[1] = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+        WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        WBTC = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+        tokenAddresses[0] = WETH;
+        tokenAddresses[1] = WBTC;
 
         //add priceFeeds for WETH and WBTC to priceFeedAddresses array
 
@@ -25,5 +32,16 @@ contract DSCEngineTest is Test {
         address dsc = deployCode("StableCoin.sol");
 
         engine = new DSCEngine(tokenAddresses, priceFeedAddresses, dsc);
+
+        dai = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        randomUser = makeAddr("random");
+    }
+
+    //DepositCollateral tests
+    function test_NotAllowedToken() public {
+        deal(dai, randomUser, 1 ether);
+        vm.prank(randomUser);
+        vm.expectRevert(InvalidTokenAddress.selector);
+        engine.depositCollateral(dai, 50);
     }
 }
